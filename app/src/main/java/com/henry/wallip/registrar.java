384ac54bf575d8bcbdb88fcbd51bfadcfdf81f65
HenryDiaz1202 <henrydiaz1202@gmail.com>
2021-08-14 21:40:25 -0600

    Servicio de llamadas terminado, ya se hace la llamada

--- app/src/main/java/com/henry/wallip/registrar.java
+++ app/src/main/java/com/henry/wallip/registrar.java
@@ -2,7 +2,12 @@ package com.henry.wallip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.FileObserver;
+import android.view.View;
+import android.widget.Toast;
 
 public class registrar extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class registrar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+    }
+    public String nume= "84493351";
+
+    public void llamar(){
+        try {
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel: "+nume)));
+        }
+        catch (Exception e){
+            Toast.makeText(getApplicationContext(), "No se puede llamar : "+e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public void llamar(View view) {
+        llamar();
     }
 }
