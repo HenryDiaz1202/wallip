83a6aa8656d100ab82186cdd198415c5116ce3cb
HenryDiaz1202 <henrydiaz1202@gmail.com>
2021-08-14 19:24:55 -0600

    Diseño de la interfaz principal termindo y creación de la tarjeta para mostrar contactos

--- app/src/main/java/com/henry/wallip/principal.java
+++ app/src/main/java/com/henry/wallip/principal.java
@@ -1,14 +1,54 @@
 package com.henry.wallip;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class principal extends AppCompatActivity {
+
+    ImageView imagen;
+    int contador = 0;
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        public void run() {
+            metodo_timmer();
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        imagen = (ImageView) findViewById(R.id.usuarioImg);
+
+        runnable.run();
+
+        //Toast.makeText(getApplicationContext(), ""+contador, Toast.LENGTH_SHORT).show();
+
     }
+
+    public void metodo_timmer(){
+        contador ++;
+        if (contador == 3){
+            imagen.setImageResource(R.drawable.usera);
+        }
+        if (contador == 6){
+            imagen.setImageResource(R.drawable.user);
+            contador = 0;
+        }
+        handler.postDelayed(runnable, 1000);
+    }
+
 }
